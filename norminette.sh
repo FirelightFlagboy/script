88d3e7f1a384fd56d0eb478c934287c1033fa2d3
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-08-12 10:11:39 +0200

    make more pleasant display

--- norminette.sh
+++ norminette.sh
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    norminette                                         :+:      :+:    :+:    #
+#    norminette.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: fbenneto <marvin@42.fr>                    +#+  +:+       +#+         #
+#    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/08/02 14:36:46 by fbenneto          #+#    #+#              #
-#    Updated: 2017/08/03 11:42:31 by fbenneto         ###   ########.fr        #
+#    Updated: 2017/08/12 10:09:53 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,8 +18,8 @@ do
 	result=`norminette -R CheckForbiddenSourceHeader $1`
 	echo '\n'
 	echo "********************************************"
-	echo $(date "+TIME: %H:%M:%S")
-	echo $result	
+	echo $(date "+TIME: %H:%M:%S") | tr -d '\n'
+	echo $result | sed $'s/Norme/\\\n&/g'
 	echo "********************************************"
 	echo '\n'
 	sleep 5
