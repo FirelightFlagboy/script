31ae4dd8ef9c720ef51f2ec07a28b986fdb5ee5d
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-08-12 15:49:17 +0200

    add compilette.sh in dev, change norminette.sh to make newline and tab every error found

--- compilette.sh
+++ compilette.sh
@@ -1,37 +1,31 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    norminette.sh                                      :+:      :+:    :+:    #
+#    compilette.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2017/08/02 14:36:46 by fbenneto          #+#    #+#              #
-#    Updated: 2017/08/12 15:48:23 by fbenneto         ###   ########.fr        #
+#    Created: 2017/08/12 14:31:23 by fbenneto          #+#    #+#              #
+#    Updated: 2017/08/12 15:46:52 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!/bin/bash
-
-if [ -z "$1" ]
+if [ $# -eq 0 ]
 	then
 		echo "no argument supplied"
 		exit
 fi
-if [ ! -d "$1" ]
-	then
-		echo "the directory does not exist"
-		exit
-fi
-true=1
-while [ $true = 1 ]
+for var in "$@"
 do
-	result=`norminette -R CheckForbiddenSourceHeader $1`
-	echo '\n'
-	echo "********************************************"
-	echo $(date "+TIME: %H:%M:%S")
-	echo $result | sed $'s/Norme/\\\n&/g; s/Error/\\\n\\\t&/g' | awk '/.*\.c|.*\.h/{print}'
-	echo "********************************************"
-	echo '\n'
-	clear
-	sleep 5
+	if [ ! -f $var ]
+	then
+		echo "file : $var not found."
+		exit
+	fi
+	if [ ${var: -2} != ".c" ] || [ ${var: -2} != ".h" ]
+	then
+		echo "file : $var is not a .c or .h"
+		exit
+	fi
 done
+echo "compiling $@"
