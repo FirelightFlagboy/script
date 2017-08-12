5063746d74e7453a0e96e88b728867ec5ba4ac98
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-08-12 15:58:47 +0200

    make compilette.sh done, with display

--- compilette.sh
+++ compilette.sh
@@ -6,7 +6,7 @@
 #    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/08/12 14:31:23 by fbenneto          #+#    #+#              #
-#    Updated: 2017/08/12 15:46:52 by fbenneto         ###   ########.fr        #
+#    Updated: 2017/08/12 15:57:48 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,10 +22,21 @@ do
 		echo "file : $var not found."
 		exit
 	fi
-	if [ ${var: -2} != ".c" ] || [ ${var: -2} != ".h" ]
+	if [ ${var: -2} != ".c" ] && [ ${var: -2} != ".h" ]
 	then
 		echo "file : $var is not a .c or .h"
 		exit
 	fi
 done
 echo "compiling $@"
+true=1
+while [ $true = 1 ]
+do
+	echo '\n'
+	echo "********************************************"
+	echo "$(date "+TIME: %H:%M:%S")\n"
+	echo `gcc -Wall -Wextra -Werror $@`
+	echo "********************************************"
+	echo '\n'
+	sleep 5
+done
