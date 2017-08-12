e03ef2142e37a484ef4213bdd51cc3a0477c7018
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-08-12 10:40:22 +0200

    add groutine.sh for the routine git

--- groutine.sh
+++ groutine.sh
@@ -1,36 +1,26 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    norminette.sh                                      :+:      :+:    :+:    #
+#    groutine.sh                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2017/08/02 14:36:46 by fbenneto          #+#    #+#              #
-#    Updated: 2017/08/12 10:30:27 by fbenneto         ###   ########.fr        #
+#    Created: 2017/08/12 10:19:40 by fbenneto          #+#    #+#              #
+#    Updated: 2017/08/12 10:39:43 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ -z "$1" ]
 	then
-		echo "no argument supplied"
+		echo "no argument supplied, you must enter a pharse for 'git commit -m'"
 		exit
 fi
-if [ ! -d "$1" ]
-	then
-		echo "the directory does not exist"
-		exit
-fi
-true=1
-while [ $true = 1 ]
-do
-	result=`norminette -R CheckForbiddenSourceHeader $1`
-	echo '\n'
-	echo "********************************************"
-	echo $(date "+TIME: %H:%M:%S") | tr -d '\n'
-	echo $result | sed $'s/Norme/\\\n&/g'
-	echo "********************************************"
-	echo '\n'
-	sleep 5
-done
+repo=`git remote -v | cut -c8- | sed 's/(.*)//g'|awk 'NR==1{print $1}'`
+echo "adding to repository"
+git add .
+echo "git commit $1"
+git commit -m "$1"
+echo "git push to $repo"
+git push
