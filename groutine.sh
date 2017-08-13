b7c4ecfa267e64178c2d1deaf0d79bdff5685d78
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-08-13 09:53:59 +0200

    aks for confirmation in groutine.sh

--- groutine.sh
+++ groutine.sh
@@ -6,7 +6,7 @@
 #    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/08/12 10:19:40 by fbenneto          #+#    #+#              #
-#    Updated: 2017/08/12 11:47:49 by fbenneto         ###   ########.fr        #
+#    Updated: 2017/08/13 09:53:56 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,6 +23,16 @@ echo "adding to repository\n\n\tgit add ."
 git add .
 echo "\n\tgit status :\n"
 git status
+
+
+while [ -z $rep ] && [ $rep != 'y' ] && [ $rep != 'n' ]
+do
+	read -p "Do you want to continue ? [y/n][ENTRE] :" rep
+done
+if [ $rep == 'n' ]
+then
+	exit 1
+fi
 echo "\n\tgit commit -m '$1'\n"
 git commit -m "$1"
 echo "\n\tgit push to $repo\n"
