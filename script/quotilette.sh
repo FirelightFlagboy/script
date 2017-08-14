7944fd945f6a1942b1d90ceec030161e5391460f
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-08-14 15:34:17 +0200

    add exit if file doesn't exist

--- script/quotilette.sh
+++ script/quotilette.sh
@@ -6,14 +6,19 @@
 #    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/08/14 14:31:30 by fbenneto          #+#    #+#              #
-#    Updated: 2017/08/14 15:02:43 by fbenneto         ###   ########.fr        #
+#    Updated: 2017/08/14 15:33:39 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/usr/bin/env bash
 #need a texte file to work
 
-texte=quote.txt
+texte=~/Documents/my_script/script/quote.txt
+if [ ! -f $texte ]
+then
+	echo "file : $texte not found."
+	exit
+fi
 wc=($(wc $texte))
 nbline=${wc[0]}
 rand=$(( ($RANDOM % $nbline) + 1 ))
