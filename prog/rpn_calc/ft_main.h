c5faf2f632844e4e94568c908ee50de2e087b9ea
Florian BENNETOT <fbenneto@e1r4p12.42.fr>
2017-11-16 16:52:16 +0100

    begin to work on rpn_calc correct cmd on alias

---
+++ prog/rpn_calc/ft_main.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_main.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/16 16:42:50 by fbenneto          #+#    #+#             */
+/*   Updated: 2017/11/16 16:48:50 by fbenneto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MAIN
+#define FT_MAIN
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t		ft_strlen(char *str);
+int			ft_add(/*TODO*/);
+int			ft_sub(/*TODO*/);
+int			ft_mul(/*TODO*/);
+int			ft_mod(/*TODO*/);
+int			ft_div(/*TODO*/);
+void		ft_main(char *str);
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+#endif
