9de01e3c77e5506fe3ba908c47a6e25c6af65b97
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-01-05 19:51:37 +0100

    update zshrc, finish gitc faster than git clone

--- script/gitc.sh
+++ script/gitc.sh
@@ -6,9 +6,16 @@
 #    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/01/05 16:35:22 by fbenneto          #+#    #+#              #
-#    Updated: 2018/01/05 18:38:16 by fbenneto         ###   ########.fr        #
+#    Updated: 2018/01/05 19:50:01 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+GIT_USER=$(git config user.name)
+GIT_MAIL=$(git config user.email)
+REP_URL="github.com"
+REPO_HTTPS="https://$REP_URL/$GIT_USER/$1"
+REPO_SSH="git@$REP_URL:$GIT_USER/$1.git"
+USE_SSH=1
 
 NC="\033[0m"
 RED="\033[31m"
@@ -26,12 +33,18 @@ error() {
 }
 
 status() {
-    printf "username  :$GIT_USER\n"
-    printf "repo name :$1\n"
+    printf "Status :\n"
+    printf "\tusername  : $GREEN$GIT_USER$NC\n"
+    printf "\temail     : $GREEN$GIT_MAIL$NC\n"
+    printf "\tssh url   :$CYAN $REPO_SSH$NC\n"
+    printf "\thttps url :$CYAN $REPO_HTTPS$NC\n"
 }
 
-printf $SEP
-printf "\tgitc a script for speed set of repo\n"
+ft_end() {
+    printf "\n$RED\tEND$NC\n"
+    printf $SEP
+    exit 0
+}
 
 if [ $# -eq 0 ]
 then
@@ -41,4 +54,28 @@ if [ "$GIT_USER" == "" ]
 then
     error "you must set the var$CYAN GIT_USER$NC to your$GREEN username$NC in github & co"
 fi
+printf $SEP
+printf "\ngitc a speed scritp for git clone faster than normal\n\n"
 status $1
+rep="0"
+echo ""
+while [ "$rep" != 'y' ] && [ "$rep" != 'n' ]
+do
+    read -p "All status's are correct ? [y/n][ENTER] :" rep
+done
+if [ "$rep" == 'n' ]
+then
+    ft_end
+fi
+if [ "$USE_SSH" == "1" ]
+then
+    git clone $REPO_SSH $1
+else
+   git clone $REPO_HTTPS $1
+fi
+rt=$?
+if [ "$rt" != "0" ] && [ "$rt" != "" ]
+then
+	error "error when git commit"
+fi
+ft_end
