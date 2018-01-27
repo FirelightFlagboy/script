3ea48b25942a1c5c602c88f1c835a4eb7ec8c34d
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-01-27 15:19:19 +0100

    work on my strlen

--- prog/ft_strlen/my_strlen.c
+++ prog/ft_strlen/my_strlen.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <f.benneto@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 14:55:05 by fbenneto          #+#    #+#             */
-/*   Updated: 2018/01/27 14:58:18 by fbenneto         ###   ########.fr       */
+/*   Updated: 2018/01/27 15:17:48 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,14 @@ size_t	my_ft_strlen (const char *str)
 {
 	const char *char_ptr;
 	const unsigned long int *longword_ptr;
-	unsigned long int longword, magic_bits, himagic, lomagic;
+	unsigned long int longword, himagic, lomagic;
 
 	for (char_ptr = str; ((unsigned long int) char_ptr & (sizeof (longword) - 1)) != 0; ++char_ptr)
 		if (*char_ptr == '\0')
 			return char_ptr - str;
 	longword_ptr = (unsigned long int *) char_ptr;
-	magic_bits = 0x7efefeffL;
 	himagic = 0x80808080L;
 	lomagic = 0x01010101L;
-	printf ("%ld %ld %ld\n", magic_bits, himagic, lomagic);	
-	if (sizeof (longword) > 4)
-	{
-		magic_bits = ((magic_bits << 16) << 16) | magic_bits;
-		himagic = ((himagic << 16) << 16) | himagic;
-		lomagic = ((lomagic << 16) << 16) | lomagic;
-	}
-	printf ("%ld %ld %ld\n", magic_bits, himagic, lomagic);
 	while (1)
 	{
 		longword = *longword_ptr++;
