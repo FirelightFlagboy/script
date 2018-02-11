ee1459a0013dc003e4ceb268d06ff466a5efc8e2
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-02-11 18:35:38 +0100

    add maj.bash for update my rapsberry pi

---
+++ script/maj.bash
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+if [ "$EUID" -ne 0 ]
+then
+	echo "please root needed"
+	exit 1
+fi
+echo "Begin update of system"
+echo "first apt-get update"
+apt-get update > /var/log/apt/maj-update.log
+echo "apt-get update done"
+echo "begin apt-get upgrade"
+apt-get upgrade > /var/log/apt/maj-upgrade.log
+echo "apt-get upgrade done"
+echo -e "you can see log at\n/var/log/apt/maj-update.log for update one"
+echo -e "or at\n/var/log/maj-upgrade.log for upgrade one"
+echo -e "\n\tupdate of system done"
