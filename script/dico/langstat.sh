39ef7dcb51125554f038fbcf2db1716884c57474
Florian BENNETOT <fbenneto@e1r4p12.42.fr>
2018-02-13 16:42:06 +0100

    save

--- script/dico/langstat.sh
+++ script/dico/langstat.sh
@@ -1,42 +1,121 @@
 #!/bin/bash
 
-declare -A letter
+file=""
+res=""
+gr=0
+csv=0
+csv_file=""
 
 error() {
 	echo $*
 	exit 1
 }
 
+usage() {
+	printf "langstat <dico file>\n"
+	echo "options:"
+	echo "-h --help								show option"
+	echo "-g --graphique-dysplay				dysplay as a graph"
+	echo "-f <FILE> --file=FILE					use the given file as dictionary"
+	echo "-c <FILE> --csv-output-file=FILE		ouput a csv format file"
+	exit 0
+}
+
 dysplay() {
-	res=""
+	printf "done:\n$res\n"
+	exit 0
+}
+
+create_cvs() {
+	cmnd="printf \"$res\" | sed -e \"s/ - /-/g\" | sed -e \"s/\([a-zA-Z]\)/\1+/g\" | tr '+' '\n' | sed -e \"s/^[ ]*//\" | sed -e \"s/^\(.*\)-\(.*\)$/\2-\1/g\""
+	eval res=\`$cmnd\`
+	echo "letter, count" > $csv_file
+	for line in $res; do
+		echo $line | sed -e "s/-/, /" >> $csv_file
+	done
+	exit 0
+}
+
+work() {
 	for l in {a..z}
 	do
-		le=${letter[$l]}
-		r=`echo "$le - $l"`
-		res="$res$r"
+		cmnd="fgrep -oi \"$l\" $file | wc -l"
+		eval r=\`$cmnd\`
+		res="$r - $l\n$res"
 	done
-	cmnd="echo -e $res | sed -e 's/\([a-zA-Z]\)/\1\n/g' | sort -k1 -nr"
+	cmnd="printf \"$res\n\" | sed -e \"s/^[ \\\\t]*//\" | sort -k1 -nr"
 	eval res=\`$cmnd\`
-	echo -e "done:\n$res"
 }
 
 main() {
-	line=`wc -l $1`
-	echo "file: $1 line: $line"
-	for l in {a..z}
-	do
-		cmnd="fgrep -oi \"$l\" $1 | wc -l"
-		eval r=\`$cmnd\`
-		letter[$l]=$r
-	done
-	dysplay
+	cmnd="awk 'END{print NR}' $file"
+	eval line=\`$cmnd\`
+	echo "file: $file line: $line"
+	work
+	dysplay &
+	pidd=$!
+	if [ $csv -eq 1 ]; then
+		create_cvs &
+		pidc=$!
+		wait $pidc
+	fi
+	wait $pidd
 }
 
-if [ $# -eq 0 ]
+while test $# -gt 0; do
+	case "$1" in
+		-h|--help)
+			usage
+			;;
+		-g|--graphique-dysplay)
+			gr=1
+			shift
+			;;
+		-f)
+			shift
+			if test $# -gt 0; then
+				file=$1
+			else
+				error "no input file specified"
+			fi
+			shift
+			;;
+		--file*)
+			file=`echo $1 | sed -e 's/^[^=]*=//g'`
+			if [ ${#file} -eq 0 ]; then
+				error "no filename suplied"
+			fi
+			;;
+		-c)
+			shift
+			if test $# -gt 0; then
+				csv=1
+				csv_file=$1
+			else
+				error "no output file specified"
+			fi
+			shift
+			;;
+		--csv-output-file*)
+			csv=1
+			csv_file=`echo $1 | sed -e 's/^[^=]*=//g'`
+			if [ ${#csv_file} -eq 0 ]; then
+				error "no filename suplied"
+			fi
+			shift
+			;;
+		*)
+			file=$1
+			break
+			;;
+	esac
+done
+
+if [ ${#file} -eq 0 ]
 then
-	error "no argument"
+	error "no file supplied"
 fi
-if [ -f $1 ]
+if [ -f $file ]
 then
 	main $1
 else
---
+++ script/gitc_old.sh
@@ -0,0 +1,81 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    gitc.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/01/05 16:35:22 by fbenneto          #+#    #+#              #
+#    Updated: 2018/01/25 14:00:05 by fbenneto         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+GIT_USER="FirelightFlagboy"
+GIT_MAIL="firelight.flagboy@gmail.com"
+REP_URL="github.com"
+REPO_HTTPS="https://$REP_URL/$GIT_USER/$1"
+REPO_SSH="git@$REP_URL:$GIT_USER/$1.git"
+USE_SSH=1
+
+NC="\033[0m"
+RED="\033[31m"
+BOL="\033[1m"
+CYAN="\033[36m"
+GREEN="\033[32m"
+SEP="\n***********************************************\n"
+
+error() {
+    if [ $# -ne 0 ]
+    then
+    	printf "$1\n"
+    fi
+	exit 0	
+}
+
+status() {
+    printf "Status :\n"
+    printf "\tusername  : $GREEN$GIT_USER$NC\n"
+    printf "\temail     : $GREEN$GIT_MAIL$NC\n"
+    printf "\tssh url   :$CYAN $REPO_SSH$NC\n"
+    printf "\thttps url :$CYAN $REPO_HTTPS$NC\n"
+}
+
+ft_end() {
+    printf "\n$RED\tEND$NC\n"
+    printf $SEP
+    exit 0
+}
+
+if [ $# -eq 0 ]
+then
+    error "missing argument for$RED git clone$NC"
+fi
+if [ "$GIT_USER" == "" ]
+then
+    error "you must set the var$CYAN GIT_USER$NC to your$GREEN username$NC in github & co"
+fi
+printf $SEP
+printf "\ngitc a speed scritp for git clone faster than normal\n\n"
+status $1
+rep="0"
+echo ""
+while [ "$rep" != 'y' ] && [ "$rep" != 'n' ]
+do
+    read -p "All status's are correct ? [y/n][ENTER] :" rep
+done
+if [ "$rep" == 'n' ]
+then
+    ft_end
+fi
+if [ "$USE_SSH" == "1" ]
+then
+    git clone $REPO_SSH $1
+else
+   git clone $REPO_HTTPS $1
+fi
+rt=$?
+if [ "$rt" != "0" ] && [ "$rt" != "" ]
+then
+	error "error when git commit"
+fi
+ft_end
