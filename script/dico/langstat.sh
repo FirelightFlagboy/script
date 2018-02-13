2dacab2ec3f94940c6e4cd1e658a5cf472fdd3b5
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-02-13 20:11:08 +0100

    w

--- script/dico/langstat.sh
+++ script/dico/langstat.sh
@@ -37,7 +37,9 @@ create_cvs() {
 }
 
 graphique() {
-	echo "	a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z"
+	echo "" > ouput
+	echo "______________________________________________________________________________" >> output
+	echo "  a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z" >> output
 }
 
 work() {
