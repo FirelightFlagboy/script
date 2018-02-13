3df63e5fad0c0ead42229f1559e1077a143fe09f
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-02-13 22:15:53 +0100

    w

--- script/dico/langstat.sh
+++ script/dico/langstat.sh
@@ -38,8 +38,10 @@ create_cvs() {
 
 create_line() {
 	i=0
+	printf "$res"
 	for l in {a..z}; do
 		cmnd="printf \"$res\" | grep -i \"$l\" | sed \"s/[^0-9]*//g\""
+		printf "$cmnd"
 		eval cr=\`$cmnd\`
 		printf "$cr\n"
 		i=$(($i+1))
