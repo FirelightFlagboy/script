7859fdae1b77cdcf38516c598ff7bb5c68bcdc7a
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-04-05 11:42:54 +0200

    test rework groutin\nmodified:   script/groutine.sh new file:   script/to/tt

--- script/groutine.sh
+++ script/groutine.sh
@@ -3,14 +3,14 @@
 #                                                         :::      ::::::::    #
 #    groutine.sh                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: fbenneto <f.benneto@student.42.fr>         +#+  +:+       +#+         #
+#    By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/08/12 10:19:40 by fbenneto          #+#    #+#              #
-#    Updated: 2018/02/25 16:05:10 by fbenneto         ###   ########.fr        #
+#    Updated: 2018/04/05 11:42:26 by fbenneto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!/usr/bin/env bash
+#!/bin/bash
 
 repo=`git remote -v | cut -c8- | sed 's/(.*)//g'|awk 'NR==1{print $1}'`
 
@@ -34,16 +34,26 @@ if [ -z "$1" ]
 		printf "no argument supplied, you must enter a pharse for 'git commit -m'\n"
 		exit 1
 fi
+
 printf $SEP
+
+# move to the root of the repo
 cd $(git rev-parse --show-toplevel || echo ".")
-printf "adding to repository\n\n\tgit add .\n"
-git add .
+
+printf "adding to repository\n\n\tgit add -A\n"
+git add -A
 printf "\n\tgit status :\n"
 git status
+cmnd="git status | grep -i 'modified\|new file' | tr -d '\t'"
+eval res=\`$cmnd\`
+echo
+
+res="$1\n$res"
+printf "$res\n"
 
 printf "$BOL$RED"
 printf "\tyou are going to push with this phrase >>$CYA$BOL"
-echo -n "$1"
+printf "$res"
 printf "$RED<<$NC\n"
 while [ "$rep" != 'y' ] && [ "$rep" != 'n' ]
 do
@@ -53,8 +63,8 @@ if [ $rep == 'n' ]
 then
 	ft_end
 fi
-printf "\n$GRE\tgit commit -m '$1'$NC\n\n"
-git commit -m "$1"
+printf "\n$GRE\tgit commit -m '$res'$NC\n\n"
+git commit -m "$res"
 printf "\n$GRE\tgit push to $repo$NC\n\n"
 git push
 ft_end
